a67f2c5c31b17266e25b1233e87049d51b094118
Shashwat Pathak <shashwatpathak18@gmail.com>
2024-10-25 18:05:32 +0530

    Added files ✅

--- src/components/CurrencyTable.jsx
+++ src/components/CurrencyTable.jsx
@@ -37,7 +37,23 @@ function CurrencyTable({
                   width={32}
                 />
               </td>
-              <td></td>
+              <td>
+                {" "}
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="size-6 rotate-90"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M3 7.5 7.5 3m0 0L12 7.5M7.5 3v13.5m13.5 0L16.5 21m0 0L12 16.5m4.5 4.5V7.5"
+                  />
+                </svg>
+              </td>
               <td>
                 <img className="h-12 col-span-2" src={getFlagUrl(toCurrency)} />{" "}
               </td>
