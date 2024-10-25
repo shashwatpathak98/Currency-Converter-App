56568cee342b8ee4bdd490c7b17c7eda684a853e
Shashwat Pathak <shashwatpathak18@gmail.com>
2024-10-26 00:28:32 +0530

    Added files ✅

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ export default function App() {
   const [baseCurrency, setBaseCurrency] = useState("USD"); // Initial currency selection
   const [targetCurrency, setTargetCurrency] = useState("INR"); // Initial currency selection
   const [rates, setRates] = useState({});
+  const [updatedTargetCurrency , setUpdatedTargetCurrency] = useState(null);
 
   //Supported Currencies based on the mapping above
   const currencies = Object.keys(currencyToCountryCode);
@@ -41,9 +42,13 @@ export default function App() {
 
   const swapCountry = () => {
     setBaseCurrency(targetCurrency);
-
     setTargetCurrency(baseCurrency);
   };
+
+  const baseCurrencyChange = (event) => {
+     const value = event.target.value;
+     setUpdatedTargetCurrency(value);
+  }
 
   return (
     <>
@@ -65,7 +70,10 @@ export default function App() {
           toCurrency={targetCurrency}
           rates={rates}
           currencyToCountryCode={currencyToCountryCode}
+         
         />
+
+        
       </div>
     </>
   );
--- src/components/CurrencyTable.jsx
+++ src/components/CurrencyTable.jsx
@@ -1,14 +1,32 @@
+import { useEffect, useState } from "react";
+
 function CurrencyTable({
   fromCurrency,
   toCurrency,
   rates,
   currencyToCountryCode,
+  
 }) {
   const conversionRate = rates[toCurrency];
+  const [displayNewTarget, setDisplayNewTarget] = useState(null);
+  const [displayNewBase, setDisplayNewBase] = useState(null);
+  
 
   const getFlagUrl = (currencyCode) => {
     return `https://flagsapi.com/${currencyToCountryCode[currencyCode].code}/shiny/32.png`;
   };
+
+
+  const setTargetCurrency = (event) => {
+    const newTargetCurrency = Number(event.target.value) * rates[toCurrency];
+    console.log(newTargetCurrency);
+
+    setDisplayNewTarget(newTargetCurrency);
+    
+  };
+
+
+
 
   return (
     <div className="overflow-x-auto">
@@ -67,6 +85,63 @@ function CurrencyTable({
           No ${fromCurrency} exchange rate for {toCurrency}
         </p>
       )}
+
+      <div className="flex flex-col items-center justify-center text-2xl">
+        <div className="flex items-center gap-2">
+          <div>{fromCurrency}</div>
+          <div>
+            <img src={getFlagUrl(fromCurrency)} />
+          </div>
+          <div className="flex items-start ">
+            <input
+              type="number"
+              className=" pl-1 input input-bordered input-accent w-full max-w-xs"
+              onChange={setTargetCurrency}
+              defaulValue={0}
+              autoFocus
+              
+              
+            />
+          </div>
+        </div>
+        <button >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="size-6 m-3"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M3 7.5 7.5 3m0 0L12 7.5M7.5 3v13.5m13.5 0L16.5 21m0 0L12 16.5m4.5 4.5V7.5"
+            />
+          </svg>
+        </button>
+
+        {/* <input
+              type="text"
+              value={displayNewTarget}
+              className="w-full max-w-xs"
+              readOnly
+            /> */}
+        <div className="flex items-center gap-2">
+          <div>{toCurrency}</div>
+          <div>
+            <img src={getFlagUrl(toCurrency)} />
+          </div>
+          <div className="flex items-start ">
+          <input
+              type="text"
+              defaultValue={displayNewTarget}
+              className=" pl-1 input input-bordered input-accent w-full max-w-xs"
+              readOnly
+            /> 
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
